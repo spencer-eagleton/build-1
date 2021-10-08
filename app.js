b46ac31d3a5bf77d55c0d7f54d61ab3913d062a0
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-08 13:16:36 -0700

    built HTML and declared variables in JS, began event listener

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // import functions and grab DOM elements
+const generateButton = document.getElementById('generate');
+const randomNumberDisplay = document.getElementById('random-num');
 
 // initialize global state
+let randomNumber = '#';
 
 // set event listeners 
+generateButton.addEventListener('click', )
+
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
